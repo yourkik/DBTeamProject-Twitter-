5824094345a9c2de929d78979093a6b03675306a
yourkik <73531665+yourkik@users.noreply.github.com>
2023-11-23 12:55:50 +0900

    Sign up
    Make SignUp method

--- Twitter.java
+++ Twitter.java
@@ -2,6 +2,8 @@ package myDB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
@@ -10,67 +12,64 @@ import java.sql.ResultSetMetaData;
 import java.sql.DatabaseMetaData;
 
 public class Twitter {
-	public static void main(String[] args) {
+    private static Connection con;
 
-		Connection con = null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/Twitter";
-			String user = "root", passwd = "12345";
-			con = DriverManager.getConnection(url, user, passwd);
-			System.out.println(con);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+    public static void signUp(String userID, String name, String email, String password) {
+        String insertUserQuery = "INSERT INTO User (UserID, Name, Email, Password) VALUES (?, ?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(insertUserQuery);
+            preparedStatement.setString(1, userID);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, password);
+            preparedStatement.executeUpdate();
+            System.out.println("회원가입이 완료되었습니다.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println("회원가입 중 오류가 발생했습니다.");
+        }
+    }
 
-		Statement stmt = null;
-		ResultSet rs = null;
-		try {
-			stmt = con.createStatement();
-			String select = "select * from Timeline";
-			rs = stmt.executeQuery(select);
-			while(rs.next()) {
-				String UserID = rs.getString(1);
-				if (rs.wasNull()) UserID = "null";
-				String timestamp = rs.getString(2);
-				if(rs.wasNull()) timestamp="null";
-				String TweetID = rs.getString(3);
-				if (rs.wasNull()) TweetID = "null";
-				System.out.println("UserID : "+UserID + ", timestamp : " + timestamp+", TweetID : "+ TweetID);
-			}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		try {
-			if (stmt != null && !stmt.isClosed())
-				stmt.close();
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
+    public static void main(String[] args) {
+        con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://127.0.0.1:3306/Twitter";
+            String user = "root", passwd = "12345";
+            con = DriverManager.getConnection(url, user, passwd);
+            System.out.println(con);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
-//		PreparedStatement pstmt = null;
-//		try {
-//			String psql = "insert into instructor value (?, ?, ?, ?)";
-//			pstmt = con.prepareStatement(psql);
-//			String id = "12345", name = "Neumann", dept_name = "Biology";
-//			int salary = 98000;
-//			pstmt.setString(1, id);
-//			pstmt.setString(2, name);
-//			pstmt.setString(3, dept_name);
-//			pstmt.setInt(4, salary);
-//			int count = pstmt.executeUpdate();
-//			System.out.println(count);
-//		} catch (SQLException e1) {
-//			e1.printStackTrace();
-//		}
-//
-//		try {
-//			if (pstmt != null && !pstmt.isClosed())
-//				pstmt.close();
-//		} catch (SQLException e1) {
-//			e1.printStackTrace();
-//		}
-	}
+        // SignUP
+        signUp("202235041", "박건우2", "yourkik@gachon.ac.kr", "12345");
+
+        // Selection
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            stmt = con.createStatement();
+            String select = "select * from Timeline";
+            rs = stmt.executeQuery(select);
+            while (rs.next()) {
+                String UserID = rs.getString(1);
+                if (rs.wasNull()) UserID = "null";
+                String timestamp = rs.getString(2);
+                if (rs.wasNull()) timestamp = "null";
+                String TweetID = rs.getString(3);
+                if (rs.wasNull()) TweetID = "null";
+                System.out.println("UserID : " + UserID + ", timestamp : " + timestamp + ", TweetID : " + TweetID);
+            }
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        } finally {
+            try {
+                if (stmt != null && !stmt.isClosed())
+                    stmt.close();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
 }
