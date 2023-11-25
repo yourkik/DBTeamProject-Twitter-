f0651ae130ed35bee9202bb0e5f1b9b5721864b3
yourkik <73531665+yourkik@users.noreply.github.com>
2023-11-25 10:24:48 +0900

    Error for Timeline and warning to User table
    구글 문서에 올리신 대로 바꾸었는데 Timeline 부분에서 error가 발생합니다. Timeline 아래 부분은 아래 작동이 되지않아 확인되지 않으니 확인 부탁드립니다

--- Twitter.sql
+++ Twitter.sql
@@ -10,14 +10,21 @@ CREATE TABLE User (
   PRIMARY KEY (UserID)
 ) DEFAULT CHARACTER SET UTF8;
 
+#Tweet하고 writer로 나눈것 하나와 마지막에 그냥 Tweet만 적어 놓으신게 있는데 뭐가 맞는지 모르겠네요
+CREATE TABLE Writer (
+  WriterID varchar(20) PRIMARY KEY,
+  Name varchar(50),
+  Email varchar(50)
+);
+
 CREATE TABLE Tweet (
   TweetID varchar(20),
   WriterID varchar(20),
   Content varchar(255),
   Timestamp varchar(25),
   PRIMARY KEY (TweetID),
-  Foreign key (writerID) references User(UserID)
-) DEFAULT CHARACTER SET UTF8;
+  FOREIGN KEY (WriterID) REFERENCES Writer(WriterID)
+);
 
 CREATE TABLE Timeline (
   UserID varchar(20),
@@ -27,37 +34,43 @@ CREATE TABLE Timeline (
   Foreign key(TweetID) references Tweet(TweetID)
 ) DEFAULT CHARACTER SET UTF8;
 
-CREATE TABLE BlackList (
-  FollowID varchar(20),
-  Timestamp varchar(25),
-  PRIMARY KEY (FollowID)
-) DEFAULT CHARACTER SET UTF8;
+CREATE TABLE Follower (
+   UserID varchar(20),
+   followingID varchar(20),
+   Timestamp varchar(25),
+   Primary key (UserID,FollowingID),
+   Foreign key (followingID) references User(UserID),
+   Foreign key(UserID) references USer(USerID)
+)default character	set UTF8;
 
 CREATE TABLE Following (
-	UserID varchar(20),
-    FollowerID varchar(20),
-    Primary key (UserID,FollowerID),
-    Foreign key (FollowerID) references User(UserID),
-    Foreign key(UserID) references User(USerID)
-) DEFAULT CHARACTER SET UTF8;
+  UserID varchar(20),
+  FollowerID varchar(20),
+  Timestamp varchar(25),
+  Primary key (UserID, FollowerID),
+  Foreign key (followerID) references User(UserID),
+  Foreign Key(UserID) references User(UserID)
+)default character set UTF8;
 
-CREATE TABLE Follower (
-	UserID varchar(20),
-    FollowingID varchar(20),
-    Primary key (UserID,FollowingID),
-    Foreign key (FollowingID) references User(UserID),
-    Foreign key(UserID) references User(USerID)
-) DEFAULT CHARACTER SET UTF8;
+CREATE TABLE BlackList (
+  BlockingUserID varchar(20),
+  BlockedUserID varchar(20),
+  Timestamp varchar(25),
+  Primary Key (BlackingUserID, BlockedUserID),
+  Foreign Key (BlockingUserID) references User(UserID),
+  Foreign Key (BlockedUserID) references User(UserID)
+)DEFAULT CHARACTER SET UTF8;
 
 CREATE TABLE Comment (
-  CommentID varchar(20),
+  CommentID varchar(20) PRIMARY KEY,
   TweetID varchar(20),
-  WriterID varchar(20),
+  UserID varchar(20),
   Content varchar(50),
-  PRIMARY KEY (CommentID),
-  foreign key(TweetID) references Tweet(TweetID),
-  foreign key(WriterID) references Timeline(UserID)
-) DEFAULT CHARACTER SET UTF8;
+  Timestamp varchar(25),
+  FOREIGN KEY (TweetID) REFERENCES Tweet(TweetID),
+  FOREIGN KEY (UserID) REFERENCES User(UserID)
+)DEFAULT CHARACTER SET UTF8;
+
 
 /*User 추가*/
 insert into User(UserID, Name, email, password) values('202235040', 'ParkGeonwoo','yourkik@gachon.ac.kr', '12345'); 
